b16f2ee3d27b3bf300e0842d6579c929c139be34
jpmrs1313 <49274169+jpmrs1313@users.noreply.github.com>
2022-02-16 22:18:17 +0000

    Update networks.py

--- networks.py
+++ networks.py
@@ -8,7 +8,6 @@ def ssim_loss(gt, y_pred, max_val=1.0):
 
 def autoencoder(shape, latentDim=100):
     height, width, depth = shape
-
     encoder = Sequential(
         [
             layers.Flatten(input_shape=(height, width, depth)),
@@ -28,7 +27,7 @@ def autoencoder(shape, latentDim=100):
             layers.LeakyReLU(),
         ]
     )
-
+    
     decoder = Sequential(
         [
             layers.Dense(64, input_shape=(latentDim,)),
@@ -48,15 +47,41 @@ def autoencoder(shape, latentDim=100):
             layers.Reshape((height, width, depth)),
         ]
     )
-
+    
     img = layers.Input(shape=(height, width, depth))
     latent_vector = encoder(img)
     output = decoder(latent_vector)
     
     model = Model(inputs=img, outputs=output)
-
-    optimizer = optimizers.Adam(learning_rate=2e-4, decay=1e-5)
     
+    # input_img = layers.Input(shape=(height,width,depth))
+
+    # h = layers.Conv2D(32, (4, 4), strides=2, activation=layers.LeakyReLU(alpha=0.2), padding='same')(input_img)
+    # h = layers.Conv2D(32, (4, 4), strides=2, activation=layers.LeakyReLU(alpha=0.2), padding='same')(h)
+    # # h = layers.Conv2D(32, (4, 4), strides=2, activation=layers.LeakyReLU(alpha=0.2), padding='same')(h)
+    # h = layers.Conv2D(32, (3, 3), strides=1, activation=layers.LeakyReLU(alpha=0.2), padding='same')(h)
+    # h = layers.Conv2D(64, (4, 4), strides=2, activation=layers.LeakyReLU(alpha=0.2), padding='same')(h)
+    # h = layers.Conv2D(64, (3, 3), strides=1, activation=layers.LeakyReLU(alpha=0.2), padding='same')(h)
+    # h = layers.Conv2D(128, (4, 4), strides=2, activation=layers.LeakyReLU(alpha=0.2), padding='same')(h)
+    # h = layers.Conv2D(64, (3, 3), strides=1, activation=layers.LeakyReLU(alpha=0.2), padding='same')(h)
+    # h = layers.Conv2D(32, (3, 3), strides=1, activation=layers.LeakyReLU(alpha=0.2), padding='same')(h)
+    # encoded = layers.Conv2D(latentDim, (8, 8), strides=1, activation='linear', padding='valid')(h)
+
+    # h = layers.Conv2DTranspose(32, (8, 8), strides=1, activation=layers.LeakyReLU(alpha=0.2), padding='valid')(encoded)
+    # h = layers.Conv2D(64, (3, 3), strides=1, activation=layers.LeakyReLU(alpha=0.2), padding='same')(h)
+    # h = layers.Conv2D(128, (3, 3), strides=1, activation=layers.LeakyReLU(alpha=0.2), padding='same')(h)
+    # h = layers.Conv2DTranspose(64, (4, 4), strides=2, activation=layers.LeakyReLU(alpha=0.2), padding='same')(h)
+    # h = layers.Conv2D(64, (3, 3), strides=1, activation=layers.LeakyReLU(alpha=0.2), padding='same')(h)
+    # h = layers.Conv2DTranspose(32, (4, 4), strides=2, activation=layers.LeakyReLU(alpha=0.2), padding='same')(h)
+    # h = layers.Conv2D(32, (3, 3), strides=1, activation=layers.LeakyReLU(alpha=0.2), padding='same')(h)
+    # h = layers.Conv2DTranspose(32, (4, 4), strides=2, activation=layers.LeakyReLU(alpha=0.2), padding='same')(h)
+    # # h = layers.Conv2DTranspose(32, (4, 4), strides=2, activation=layers.LeakyReLU(alpha=0.2), padding='same')(h)
+
+    # decoded = layers.Conv2DTranspose(depth, (4, 4), strides=2, activation='sigmoid', padding='same')(h)
+
+    # model=Model(input_img, decoded)
+    optimizer = optimizers.Adam(learning_rate=2e-4, decay=1e-5)
+
     model.compile(optimizer=optimizer, loss=ssim_loss, metrics=["mae"])
 
     return model
