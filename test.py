6c2c275d82b61c6577ecd91665ce1550883b5b0f
jpmrs1313 <49274169+jpmrs1313@users.noreply.github.com>
2022-02-16 22:22:42 +0000

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import glob
 import numpy as np
 import cv2
-from utils import load_image, pre_process, get_residual_map
+from utils import load_image, pre_process, get_residual_map, extract_patches, recon_im
 from networks import ssim_loss
 from options import Options
 from sklearn.metrics import roc_auc_score, auc
@@ -12,6 +12,7 @@ from sklearn.metrics import roc_auc_score, auc
 cfg = Options().parse()
 
 image_shape = (cfg.image_size, cfg.image_size,3)
+patch_shape = (cfg.patch_size, cfg.patch_size, 3)
 
 # read all image file paths
 image_paths = []
@@ -52,23 +53,12 @@ autoencoder = tf.keras.models.load_model("model2", compile=False)
 optimizer = tf.keras.optimizers.Adam(learning_rate=2e-4, decay=1e-5)
 autoencoder.compile(optimizer=optimizer, loss=ssim_loss, metrics=["mae"])
 
-final_x, final_y = [], []
-
-score = []
 for image,y_true in ds:
-    y_true=y_true.numpy().astype(np.uint8)
-    ret2,y_true = cv2.threshold(y_true,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-    y_true= y_true /255.0
-    y_true=y_true.astype(np.uint8)
-  
-    ssim_residual_map = get_residual_map(image,autoencoder)
+
+    ssim_residual_map,l1_residual_map = get_residual_map(image,autoencoder)
     y_pred = np.zeros((256,256), np.uint8)
-    y_pred[ssim_residual_map > 0.19186024546623237] = 1
-    
-    print(np.unique(y_pred))
-    print(np.unique(y_true))
-    print(type(y_pred), y_pred.dtype)
-    print(type(y_true), y_true.dtype)
+    y_pred[ssim_residual_map > 0.23572287082672116] = 1
+    #y_pred[l1_residual_map > 0.00021582189743639965] = 1
 
     fig = plt.figure(figsize=(10, 7))
     rows = 1
@@ -85,33 +75,7 @@ for image,y_true in ds:
     plt.axis("off")
     plt.show()
 
-    y_pred = y_pred.flatten()
-    y_true = y_true.flatten()
 
-    print(roc_auc_score(y_pred, y_true))
-
-
-# for result, ds_element in zip(results,ds):
-#     image, ground_truth = ds_element
-
-#     ssim_residual_map = get_residual_map(image,autoencoder, cfg)
-#     mask = np.zeros((256,256))
-#     mask[ssim_residual_map > 0.19186024546623237] = 1
-    
-#     fig = plt.figure(figsize=(10, 7))
-#     rows = 1
-#     columns = 2
-#     fig.add_subplot(rows, columns, 1)
-
-#     plt.imshow(mask,cmap='gray')
-#     plt.title("prediction")
-#     plt.axis("off")
-
-#     fig.add_subplot(rows, columns, 2)
-#     plt.imshow(ground_truth,cmap='gray')
-#     plt.title("ground_truth")
-#     plt.axis("off")
-#     plt.show()
 
 
 
